634ed8a1d0a1323a04195cbc191df366fe717466
Pedro Plasencia <programacion.en.esp@gmail.com>
2022-12-12 19:51:16 +0100

    feat: improve in readability

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,14 +1,14 @@
-const TREE_CONTAINER = document.createElement("pre");
-const MAIN_CONTAINER = document.querySelector(".main-container");
+const treeContainer = document.createElement("pre");
+const mainContainer = document.querySelector(".main-container");
 
 const createTree = (size) => {
   for (let i = 0; i < size; i++) {
-    const SPAN_ELEMENT = document.createElement("span");
-    const BRANCH = i === 0 || i === size - 1 ? "*\n" : `${"*".repeat(i * 2)}\n`;
-    SPAN_ELEMENT.textContent = BRANCH;
-    TREE_CONTAINER.appendChild(SPAN_ELEMENT);
+    const spanElement = document.createElement("span");
+    const branch = i === 0 || i === size - 1 ? "*\n" : `${"*".repeat(i * 2)}\n`;
+    spanElement.textContent = branch;
+    treeContainer.appendChild(spanElement);
   }
-  MAIN_CONTAINER.appendChild(TREE_CONTAINER);
+  mainContainer.appendChild(treeContainer);
 };
 
 const getRandomValue = (max, min = 1) => {
@@ -17,19 +17,21 @@ const getRandomValue = (max, min = 1) => {
 
 const createSnow = (density) => {
   for (let i = 0; i < density; i++) {
-    const SNOW_FLAKE = document.createElement("span");
-    const RANDOM_HORIZONTAL_POSITION = `${getRandomValue(100)}%`;
-    const RANDOM_FALL_DELAY = `${getRandomValue(100)}s`;
-    const RANDOM_FALL_DURATION = `${getRandomValue(20, 5)}s`;
-    const RANDOM_FLAKE_SIZE = `${getRandomValue(7, 1)}px`;
-    const RANDOM_FLAKE_OPACITY = Math.random().toFixed(2);
-    SNOW_FLAKE.classList.add("snow");
-    SNOW_FLAKE.style.opacity = RANDOM_FLAKE_OPACITY;
-    SNOW_FLAKE.style.width = RANDOM_FLAKE_SIZE;
-    SNOW_FLAKE.style.height = RANDOM_FLAKE_SIZE;
-    SNOW_FLAKE.style.animation = `fall ${RANDOM_FALL_DURATION} ${RANDOM_FALL_DELAY} linear infinite`;
-    SNOW_FLAKE.style.right = RANDOM_HORIZONTAL_POSITION;
-    MAIN_CONTAINER.appendChild(SNOW_FLAKE);
+    const snowFlake = document.createElement("span");
+    const horizontalPosition = `${getRandomValue(100)}%`;
+    const fallDelay = `${getRandomValue(100)}s`;
+    const fallDuration = `${getRandomValue(20, 5)}s`;
+    const flakeSize = `${getRandomValue(7, 1)}px`;
+    const flakeOpacity = Math.random().toFixed(2);
+
+    snowFlake.classList.add("snow");
+    snowFlake.style.opacity = flakeOpacity;
+    snowFlake.style.width = flakeSize;
+    snowFlake.style.height = flakeSize;
+    snowFlake.style.animation = `fall ${fallDuration} ${fallDelay} linear infinite`;
+    snowFlake.style.right = horizontalPosition;
+
+    mainContainer.appendChild(snowFlake);
   }
 };
 
